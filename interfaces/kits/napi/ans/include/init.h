92877afa56da56de5bf872f7006b2ce943764979
zhaoyuan17 <zhaoyuan17@huawei.com>
2021-09-17 15:05:50 +0800

    fix codex and d.ts
    Signed-off-by: zhaoyuan17 <zhaoyuan17@huawei.com>

--- interfaces/kits/napi/ans/include/init.h
+++ interfaces/kits/napi/ans/include/init.h
@@ -41,7 +41,7 @@ napi_module _module = {
     .nm_flags = 0,
     .nm_filename = nullptr,
     .nm_register_func = Init,
-    .nm_modname = "notificationnapi",
+    .nm_modname = "notification",
     .nm_priv = ((void *)0),
     .reserved = {0}
 
